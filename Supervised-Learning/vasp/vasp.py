bea3ec35a267a053f31b29bcc2aa5b1bdc74a4b2
Kritshekhar <kjha9@asu.edu>
2022-11-21 15:24:51 -0700

    VASP

--- Supervised-Learning/vasp/vasp.py
+++ Supervised-Learning/vasp/vasp.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# First we need to import utils package and tensorflow with addons.
+
+# In[1]:
+
+
 from utils import *
 import tensorflow as tf
 import tensorflow_addons as tfa
-
+import tensorflow_ranking as tfr
+from matplotlib import pyplot as plt
+import pandas as pd
 set_seed(42)
+
+
+# Next, we define our model. Note that reparametrization trick is done manually - it can be done with tensorflow probability package, which is doing that by itself automatically.
+
+# In[2]:
+
 
 class DiagonalToZero(tf.keras.constraints.Constraint):
     def __call__(self, w):
@@ -20,155 +36,154 @@ class Sampling(tf.keras.layers.Layer):
         epsilon = tf.keras.backend.random_normal(shape=(batch, dim), stddev=1.)
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
-class Model(tf.keras.Model):
-    def __init__(self, num_words, latent=1024, hidden=1024, items_sampling=1.):
-        """
-        num_words             nr of items in dataset (size of tokenizer)
-        latent                size of latent space
-        hidden                size of hidden layers
-        items_sampling        Large items datatsets can be very gpu memory consuming in EASE layer.
-                              This coefficient reduces number of ease parametrs by taking only
-                              fraction of items sorted by popularity as input for model.
-                              Note: This coef should be somewhere around coverage@100 achieved by full
-                              size model.
-                              For ML20M this coef should be between 0.4888 (coverage@100 for full model)
-                              and 1.0
-                              For Netflix this coef should be between 0.7055 (coverage@100 for full
-                              model) and 1.0
-        """
-        super(Model, self).__init__()
-
-        self.sampled_items = int(num_words * items_sampling)
-
-        assert self.sampled_items > 0
-        assert self.sampled_items <= num_words
-
-        self.s = self.sampled_items < num_words
-
-        # ************* ENCODER ***********************
-        self.encoder1 = tf.keras.layers.Dense(hidden)
-        self.ln1 = tf.keras.layers.LayerNormalization()
-        self.encoder2 = tf.keras.layers.Dense(hidden)
-        self.ln2 = tf.keras.layers.LayerNormalization()
-        self.encoder3 = tf.keras.layers.Dense(hidden)
-        self.ln3 = tf.keras.layers.LayerNormalization()
-        self.encoder4 = tf.keras.layers.Dense(hidden)
-        self.ln4 = tf.keras.layers.LayerNormalization()
-        self.encoder5 = tf.keras.layers.Dense(hidden)
-        self.ln5 = tf.keras.layers.LayerNormalization()
-        self.encoder6 = tf.keras.layers.Dense(hidden)
-        self.ln6 = tf.keras.layers.LayerNormalization()
-        self.encoder7 = tf.keras.layers.Dense(hidden)
-        self.ln7 = tf.keras.layers.LayerNormalization()
-
-        # ************* SAMPLING **********************
-        self.dense_mean = tf.keras.layers.Dense(latent,
-                                                name="Mean")
-        self.dense_log_var = tf.keras.layers.Dense(latent,
-                                                   name="log_var")
-
-        self.sampling = Sampling(name='Sampler')
-
-        # ************* DECODER ***********************
-        self.decoder1 = tf.keras.layers.Dense(hidden)
-        self.dln1 = tf.keras.layers.LayerNormalization()
-        self.decoder2 = tf.keras.layers.Dense(hidden)
-        self.dln2 = tf.keras.layers.LayerNormalization()
-        self.decoder3 = tf.keras.layers.Dense(hidden)
-        self.dln3 = tf.keras.layers.LayerNormalization()
-        self.decoder4 = tf.keras.layers.Dense(hidden)
-        self.dln4 = tf.keras.layers.LayerNormalization()
-        self.decoder5 = tf.keras.layers.Dense(hidden)
-        self.dln5 = tf.keras.layers.LayerNormalization()
-
-        self.decoder_resnet = tf.keras.layers.Dense(self.sampled_items,
-                                                    activation='sigmoid',
-                                                    name="DecoderR")
-        self.decoder_latent = tf.keras.layers.Dense(self.sampled_items,
-                                                    activation='sigmoid',
-                                                    name="DecoderL")
-
-        # ************* PARALLEL SHALLOW PATH *********
-
-        self.ease = tf.keras.layers.Dense(
-            self.sampled_items,
-            activation='sigmoid',
-            use_bias=False,
-            kernel_constraint=DiagonalToZero(),  # critical to prevent learning simple identity
-        )
-
-    def call(self, x, training=None):
-        sampling = self.s
-        if sampling:
-            sampled_x = x[:, :self.sampled_items]
-            non_sampled = x[:, self.sampled_items:] * 0.
-        else:
-            sampled_x = x
-
-        z_mean, z_log_var, z = self.encode(sampled_x)
-        if training:
-            d = self.decode(z)
-            # Add KL divergence regularization loss.
-            kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
-            kl_loss = tf.reduce_mean(kl_loss)
-            kl_loss *= -0.5
-            self.add_loss(kl_loss)
-            self.add_metric(kl_loss, name="kl_div")
-        else:
-            d = self.decode(z_mean)
-
-        if sampling:
-            d = tf.concat([d, non_sampled], axis=-1)
-
-        ease = self.ease(sampled_x)
-
-        if sampling:
-            ease = tf.concat([ease, non_sampled], axis=-1)
-
-        return d * ease
-
-    def decode(self, x):
-        e0 = x
-        e1 = self.dln1(tf.keras.activations.swish(self.decoder1(e0)))
-        e2 = self.dln2(tf.keras.activations.swish(self.decoder2(e1) + e1))
-        e3 = self.dln3(tf.keras.activations.swish(self.decoder3(e2) + e1 + e2))
-        e4 = self.dln4(tf.keras.activations.swish(self.decoder4(e3) + e1 + e2 + e3))
-        e5 = self.dln5(tf.keras.activations.swish(self.decoder5(e4) + e1 + e2 + e3 + e4))
-
-        dr = self.decoder_resnet(e5)
-        dl = self.decoder_latent(x)
-
-        return dr * dl
-
-    def encode(self, x):
-        e0 = x
-        e1 = self.ln1(tf.keras.activations.swish(self.encoder1(e0)))
-        e2 = self.ln2(tf.keras.activations.swish(self.encoder2(e1) + e1))
-        e3 = self.ln3(tf.keras.activations.swish(self.encoder3(e2) + e1 + e2))
-        e4 = self.ln4(tf.keras.activations.swish(self.encoder4(e3) + e1 + e2 + e3))
-        e5 = self.ln5(tf.keras.activations.swish(self.encoder5(e4) + e1 + e2 + e3 + e4))
-        e6 = self.ln6(tf.keras.activations.swish(self.encoder6(e5) + e1 + e2 + e3 + e4 + e5))
-        e7 = self.ln7(tf.keras.activations.swish(self.encoder7(e6) + e1 + e2 + e3 + e4 + e5 + e6))
-
-        z_mean = self.dense_mean(e7)
-        z_log_var = self.dense_log_var(e7)
-        z = self.sampling((z_mean, z_log_var))
-
-        return z_mean, z_log_var, z
-
 class VASP(tf.keras.Model):
+    class Model(tf.keras.Model):
+        def __init__(self, num_words, latent=1024, hidden=1024, items_sampling=1.):
+            """
+            num_words             nr of items in dataset (size of tokenizer)
+            latent                size of latent space
+            hidden                size of hidden layers
+            items_sampling        Large items datatsets can be very gpu memory consuming in EASE layer.
+                                  This coefficient reduces number of ease parametrs by taking only
+                                  fraction of items sorted by popularity as input for model.
+                                  Note: This coef should be somewhere around coverage@100 achieved by full
+                                  size model.
+                                  For ML20M this coef should be between 0.4888 (coverage@100 for full model)
+                                  and 1.0
+                                  For Netflix this coef should be between 0.7055 (coverage@100 for full
+                                  model) and 1.0
+            """
+            super(VASP.Model, self).__init__()
+
+            self.sampled_items = int(num_words * items_sampling)
+
+            assert self.sampled_items > 0
+            assert self.sampled_items <= num_words
+
+            self.s = self.sampled_items < num_words
+
+            # ************* ENCODER ***********************
+            self.encoder1 = tf.keras.layers.Dense(hidden)
+            self.ln1 = tf.keras.layers.LayerNormalization()
+            self.encoder2 = tf.keras.layers.Dense(hidden)
+            self.ln2 = tf.keras.layers.LayerNormalization()
+            self.encoder3 = tf.keras.layers.Dense(hidden)
+            self.ln3 = tf.keras.layers.LayerNormalization()
+            self.encoder4 = tf.keras.layers.Dense(hidden)
+            self.ln4 = tf.keras.layers.LayerNormalization()
+            self.encoder5 = tf.keras.layers.Dense(hidden)
+            self.ln5 = tf.keras.layers.LayerNormalization()
+            self.encoder6 = tf.keras.layers.Dense(hidden)
+            self.ln6 = tf.keras.layers.LayerNormalization()
+            self.encoder7 = tf.keras.layers.Dense(hidden)
+            self.ln7 = tf.keras.layers.LayerNormalization()
+
+            # ************* SAMPLING **********************
+            self.dense_mean = tf.keras.layers.Dense(latent,
+                                                    name="Mean")
+            self.dense_log_var = tf.keras.layers.Dense(latent,
+                                                       name="log_var")
+
+            self.sampling = Sampling(name='Sampler')
+
+            # ************* DECODER ***********************
+            self.decoder1 = tf.keras.layers.Dense(hidden)
+            self.dln1 = tf.keras.layers.LayerNormalization()
+            self.decoder2 = tf.keras.layers.Dense(hidden)
+            self.dln2 = tf.keras.layers.LayerNormalization()
+            self.decoder3 = tf.keras.layers.Dense(hidden)
+            self.dln3 = tf.keras.layers.LayerNormalization()
+            self.decoder4 = tf.keras.layers.Dense(hidden)
+            self.dln4 = tf.keras.layers.LayerNormalization()
+            self.decoder5 = tf.keras.layers.Dense(hidden)
+            self.dln5 = tf.keras.layers.LayerNormalization()
+
+            self.decoder_resnet = tf.keras.layers.Dense(self.sampled_items,
+                                                        activation='sigmoid',
+                                                        name="DecoderR")
+            self.decoder_latent = tf.keras.layers.Dense(self.sampled_items,
+                                                        activation='sigmoid',
+                                                        name="DecoderL")
+
+            # ************* PARALLEL SHALLOW PATH *********
+
+            self.ease = tf.keras.layers.Dense(
+                self.sampled_items,
+                activation='sigmoid',
+                use_bias=False,
+                kernel_constraint=DiagonalToZero(),  # critical to prevent learning simple identity
+            )
+
+        def call(self, x, training=None):
+            sampling = self.s
+            if sampling:
+                sampled_x = x[:, :self.sampled_items]
+                non_sampled = x[:, self.sampled_items:] * 0.
+            else:
+                sampled_x = x
+
+            z_mean, z_log_var, z = self.encode(sampled_x)
+            if training:
+                d = self.decode(z)
+                # Add KL divergence regularization loss.
+                kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+                kl_loss = tf.reduce_mean(kl_loss)
+                kl_loss *= -0.5
+                self.add_loss(kl_loss)
+                self.add_metric(kl_loss, name="kl_div")
+            else:
+                d = self.decode(z_mean)
+
+            if sampling:
+                d = tf.concat([d, non_sampled], axis=-1)
+
+            ease = self.ease(sampled_x)
+
+            if sampling:
+                ease = tf.concat([ease, non_sampled], axis=-1)
+
+            return d * ease
+
+        def decode(self, x):
+            e0 = x
+            e1 = self.dln1(tf.keras.activations.swish(self.decoder1(e0)))
+            e2 = self.dln2(tf.keras.activations.swish(self.decoder2(e1) + e1))
+            e3 = self.dln3(tf.keras.activations.swish(self.decoder3(e2) + e1 + e2))
+            e4 = self.dln4(tf.keras.activations.swish(self.decoder4(e3) + e1 + e2 + e3))
+            e5 = self.dln5(tf.keras.activations.swish(self.decoder5(e4) + e1 + e2 + e3 + e4))
+
+            dr = self.decoder_resnet(e5)
+            dl = self.decoder_latent(x)
+
+            return dr * dl
+
+        def encode(self, x):
+            e0 = x
+            e1 = self.ln1(tf.keras.activations.swish(self.encoder1(e0)))
+            e2 = self.ln2(tf.keras.activations.swish(self.encoder2(e1) + e1))
+            e3 = self.ln3(tf.keras.activations.swish(self.encoder3(e2) + e1 + e2))
+            e4 = self.ln4(tf.keras.activations.swish(self.encoder4(e3) + e1 + e2 + e3))
+            e5 = self.ln5(tf.keras.activations.swish(self.encoder5(e4) + e1 + e2 + e3 + e4))
+            e6 = self.ln6(tf.keras.activations.swish(self.encoder6(e5) + e1 + e2 + e3 + e4 + e5))
+            e7 = self.ln7(tf.keras.activations.swish(self.encoder7(e6) + e1 + e2 + e3 + e4 + e5 + e6))
+
+            z_mean = self.dense_mean(e7)
+            z_log_var = self.dense_log_var(e7)
+            z = self.sampling((z_mean, z_log_var))
+
+            return z_mean, z_log_var, z
 
     def __init__(self, dataset):
         super(VASP, self).__init__()
-        self.dataset = dataset 
-        #self.name = name
+        self.dataset = dataset
+        self.history = {} 
     
     def create_model(self, latent=2048, hidden=4096, ease_items_sampling=1., summary=False):
-        self.model = Model(self.dataset.num_words, latent, hidden, ease_items_sampling)
+        self.model = VASP.Model(self.dataset.num_words, latent, hidden, ease_items_sampling)
         self.model(self.dataset.split.train_gen[0][0])
         if summary:
             self.model.summary()
-        self.mc = MetricsCallback(self)
+        #self.mc = MetricsCallback(self)
 
     def compile_model(self, lr=0.00002, fl_alpha=0.25, fl_gamma=2.0):
         """
@@ -179,16 +194,22 @@ class VASP(tf.keras.Model):
         self.model.compile(
             optimizer=tf.keras.optimizers.Nadam(lr),
             loss=lambda x, y: tfa.losses.sigmoid_focal_crossentropy(x, y, alpha=fl_alpha, gamma=fl_gamma),
-            metrics=['mse', cosine_loss]
+            metrics=[tf.keras.metrics.RootMeanSquaredError(name='root_mean_squared_error'), tfr.keras.metrics.NDCGMetric(topn=100), tfr.keras.metrics.RecallMetric(topn=20), tfr.keras.metrics.HitsMetric(topn=10), tf.keras.metrics.MeanAbsoluteError(name='mean_absolute_error')]
         )
 
     def train_model(self, epochs=150):
-        self.model.fit(
+        run_history = self.model.fit(
             self.dataset.split.train_gen,
             validation_data=self.dataset.split.validation_gen,
             epochs=epochs,
-            callbacks=[self.mc]
+            #callbacks=[self.mc]
         )
+        self.history.update(run_history.history)
+
+
+# Now, we can load previously preprocessed dataset. We also load pre-defined train/test/validation split.
+
+# In[3]:
 
 
 dataset = Data(d='', pruning='u5')
@@ -198,6 +219,15 @@ dataset.split.train_users = pd.read_json("../../data/train_users.json").userid.a
 dataset.split.validation_users = pd.read_json("../../data/val_users.json").userid.apply(str).to_frame()
 dataset.split.test_users = pd.read_json("../../data/test_users.json").userid.apply(str).to_frame()
 dataset.split.generators()
+
+
+# Now we can create an instance of our model and train it.
+# 
+# For validation during training we sample 80% of the user's interactions randomly as input for the model, and then we measure Recall@20, Recall@50 and NDCG@100 for predicted interactions against the remaining 20% of the user's interactions.
+# 
+# This method can give different results for different seeds, but since it is used for validation during training only, it's good enough.
+
+# In[4]:
 
 
 #m = VASP(dataset.split, name="VASP_ML20_1")
@@ -216,22 +246,54 @@ print("=" * 80)
 print("Than train for 20 epochs with lr 0.000001")
 m.compile_model(lr=0.00001, fl_alpha=0.25, fl_gamma=2.0)
 m.train_model(20)
-path="results.png"
-m.mc.plot_history(path=path)
+# Dataframe with the details of the training.
+
+# In[5]:
+
+
+#m.mc.get_history_df()
+
+print(m.history.keys())
+
+# And the details of the training as a plot.
+
+# In[6]:
+
+
+#m.mc.plot_history()
+pd.DataFrame(m.history).plot(figsize=(10,12))
+plt.savefig('results.png')
+
+# For final evaluation we do 5-fold validation on the user's interaction history. We think that this is more objective measure than only hide sampled 20% of the user's interactions randomly.
+
+# In[7]:
+
+
+# 5-fold evaluation on the test set
 
 test_r20s = []
 test_r50s = []
 test_n100s = []
+test_hr20s = []
 
 for fold in range(1,6):
-    ev=Evaluator(m.split, method=str(fold)+'_20')
+    ev=Evaluator(m.dataset.split, method=str(fold)+'_20')
     ev.update(m.model)
 
     test_n100s.append(ev.get_ncdg(100))
     test_r20s.append(ev.get_recall(20))
     test_r50s.append(ev.get_recall(50))
+    test_hr20s.append(ev.get_hr(20))
 
 print("TEST SET (MEAN)")
 print("5-fold mean NCDG@100", round(sum(test_n100s) / len(test_n100s),3))
 print("5-fold mean Recall@20", round(sum(test_r20s) / len(test_r20s),3))
 print("5-fold mean Recall@50", round(sum(test_r50s) / len(test_r50s),3))
+print("5-fold mean HitRatio@20", round(sum(test_hr20s) / len(test_hr20s),3))
+
+
+# In[ ]:
+
+
+
+
